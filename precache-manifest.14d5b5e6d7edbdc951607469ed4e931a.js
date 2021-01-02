61c99cbab87bbbcabc84f31ed353886b43d033cc
mrodrr18 <36730107+mrodrr18@users.noreply.github.com>
2021-01-02 17:22:37 +0100

    Arreglos

---
+++ precache-manifest.14d5b5e6d7edbdc951607469ed4e931a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fe0a855ecd3c87900f",
+    "url": "/css/app.7bd8395c.css"
+  },
+  {
+    "revision": "9ef70c9df53a0ae6bdb6",
+    "url": "/css/chunk-vendors.fe75cb7a.css"
+  },
+  {
+    "revision": "482a5e6b0886159412046b7f65fd529b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4fe0a855ecd3c87900f",
+    "url": "/js/app.f3f14c52.js"
+  },
+  {
+    "revision": "9ef70c9df53a0ae6bdb6",
+    "url": "/js/chunk-vendors.9d5f6895.js"
+  },
+  {
+    "revision": "5dc01a02fa708938e138d314c05af02b",
+    "url": "/manifest.json"
+  }
+]);
